8d4d506466939964a243d57be83da1964522dd88
Nicholas Dunham <11730795+NicholasDunham@users.noreply.github.com>
2025-04-03 15:27:35 -0700

    Enable Nix support in Emacs

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -167,7 +167,9 @@
        ;;lua               ; one-based indices? one-based indices
        ;; markdown         ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       (nix                ; I hereby declare "nix geht mehr!"
+        +lsp
+        +tree-sitter)
        ;;ocaml             ; an objective camel
        (org                ; organize your plain life in plain text
         +present
